bbc35b27c5bab2728f374f36908f736ac300ca2b
JhonRommelMuhi <jromdaily@gmail.com>
2022-04-11 10:07:53 +0800

    Fourth song clickable

---
+++ song4.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iMUSIC </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to iMUSIC</h1>
+                <p class="lead">Creating an escape from reality</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Monster</h2>";
+                        echo "<p class='lead '>Yoasobi</p>";
+                
+                        echo "<p> Ah, so what a scene, let's do another toast to confine </p>";
+                        echo "<p> Match a city, overflowing laughing voices are </p>";
+                        echo "<p> Pretended fabrications of a lie, blinded eye, deception </p>";
+                        echo "<p> I'm trying not to go mad </p>";
+                        echo "<br/>";
+
+                        echo "<p> Could a, could an aroma come and feed the brain in tune </p>";
+                        echo "<p> With stimulation, reawaken inclinations </p>";
+                        echo "<p> Don't know who's gonna go there today, I wonder </p>";
+                        echo "<br/>";
+
+                        echo "<p> If I kept trying then, what can I, that cannot be done </p>";
+                        echo "<p> On to the world, what can I, that cannot be done </p>";
+                        echo "<p> Looking in those dark little eyes, I decide </p>";
+                        echo "<p> I need to not once let your tear fall beneath </p>";
+                        echo "<br/>";
+
+                        echo "<p> Ah, get out to find it now, don't ever let go </p>";
+                        echo "<p> The brighter future knows to reach it </p>";
+                        echo "<p> Even when the world is caught and full of wrong </p>";
+                        echo "<br/>";
+
+                        echo "<p> Keep needing ya to laugh and be free for once </p>";
+                        echo "<p> No, I cannot be hurting no one </p>";
+                        echo "<p> I wanted, I wanted to be stronger now </p>";
+                        echo "<p> Woken to resume my need to stay the real me </p>";
+                        echo "<br/>";
+
+                        echo "<p> So what a scene, here's to another safely passed time</p>";
+                        echo "<p> Masking every new bad rumor flowing in the city </p>";
+                        echo "<p> Feel the lie, feel the lie, release to turn a blind eye </p>";
+                        echo "<p> Choking on their own hysteria </p>";
+                        echo "<br/>";
+
+                        echo "<p> Marching with an innocence, they proceed </p>";
+                        echo "<p> To a location echoing the sound of feet skipping and </p>";
+                        echo "<p> Feeling high, feeling high, memories of a lasting flavor </p>";
+                        echo "<p> The other side's insanity </p>";
+                        echo "<br/>";
+
+                        echo "<p> Ah… </p>";
+                        echo "<br/>";
+
+                        echo "<p> Can you call the standard good a living at all </p>";
+                        echo "<p> And can nobody feeling sadder be a living at all </p>";
+                        echo "<p> How is a straighter path you could see a living at all </p>";
+                        echo "<p> So let's imagine going no mistake as living at all </p>";
+                        echo "<br/>";
+
+                        echo "<p> And I don't wanna live as you'd assume, be saying it's right </p>";
+                        echo "<p> And artificially be living and you're saying it's right </p>";
+                        echo "<p> I don't know what I'll be, and to believe this, I wonder </p>";
+                        echo "<p> Want to know the truth, what I'm inside and under </p>";
+                        echo "<p> Oh, spill me the tale </p>";
+                        echo "<p> Oh, spill me the tale </p>";
+                        echo "<br/>";
+
+                        echo "<p> Still, we reside inside a dry confusing big world </p>";
+                        echo "<p> I make a wish alone </p>";
+                        echo "<p> Believe it, I just wanna be with you forever </p>";
+                        echo "<p> Give me hope to be just smiling with you again </p>";
+                        echo "<br/>";
+
+                        echo "<p> Ah, the skipping beating in my heart </p>";
+                        echo "<p> Is shaking me within, and begins to shout </p>";
+                        echo "<p> Now you got to move, go make the way </p>";
+                        echo "<br/>";
+
+                        echo "<p> I will rise to prove the fragile little myself </p>";
+                        echo "<p> Go bite into my own and reach it </p>";
+                        echo "<p> Even when the world is caught and full of wrong </p>";
+                        echo "<p> Keep needing ya to laugh and be free for once </p>";
+                        echo "<p> No, I cannot see nobody cry </p>";
+                        echo "<p> I wanted, I wanted to be stronger now </p>";
+                        echo "<p> Woken to resume my need to stay the real me </p>";
+                        echo "<br/>";
+
+                        echo "<p> I'm gonna keep ya from danger, there are too many </p>";
+                        echo "<p> I'm running, I'm running, I'm running for you </p>";
+                        echo "<p> I will overcome what is residing for you </p>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jhon Rommel Muhi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
