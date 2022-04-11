c437d2e5da5f83142f2d3d1e3ad1cf426410dde4
JhonRommelMuhi <jromdaily@gmail.com>
2022-04-11 10:07:00 +0800

    Third song clickable

---
+++ song3.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iMUSIC </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to iMUSIC</h1>
+                <p class="lead">Creating an escape from reality</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Pano</h2>";
+                        echo "<p class='lead '>Zack Tabuldo</p>";
+                
+                        echo "<p> Oh giliw naririnig mo ba </p>";
+                        echo "<p> Ang yong sarili </p>";
+                        echo "<p> Nakakabaliw lumalabas </p>";
+                        echo "<p> Sa yong bibig </p>";
+                        echo "<br/>";
+
+                        echo "<p> Alam kong uto uto ako </p>";
+                        echo "<p> Alam ko na marupok </p>";
+                        echo "<p> Tao lang din naman </p>";
+                        echo "<p> Kasi ako </p>";
+                        echo "<br/>";
+
+                        echo "<p> May nararamdaman din ako </p>";
+                        echo "<p> Di kasi manhid na tulad mo </p>";
+                        echo "<p> Alam kong sanay bumitaw </p>";
+                        echo "<p> Ang isang tulad mo </p>";
+                        echo "<p> Lalayo na ba ako </p>";
+                        echo "<br/>";
+
+                        echo "<p> Pano naman ako </p>";
+                        echo "<p> Nahulog na sayo </p>";
+                        echo "<p> Binitawan mo lang ba talaga ako </p>";
+                        echo "<br/>";
+
+                        echo "<p> Pano naman ako </p>";
+                        echo "<p> Naghintay ng matagal sayo </p>";
+                        echo "<p> Wala lang ba talaga lahat ng yon sayo </p>";
+                        echo "<p> Wala lang ba talaga lahat ng yon sayo Ano na bang gagawin ko </p>";
+                        echo "<br/>";
+
+                        echo "<p> Sinasadya mo ba ang lahat </p>";
+                        echo "<p> O trip mo lang ba ako saktan </p>";
+                        echo "<p> Pagtapos kong ibigay balikat ko </p>";
+                        echo "<p> Pag ika'y umiiyak </p>";
+                        echo "<br/>";
+
+                        echo "<p> Ano bang tingin mo saakin </p>";
+                        echo "<p> Isa ba akong alipin </p>";
+                        echo "<p> Wala ka bang modo </p>";
+                        echo "<p> Anong ginawa mo </p>";
+                        echo "<p> Nagtiwala naman sayo </p>";
+                        echo "<br/>";
+
+                        echo "<p> May nararamdaman din ako </p>";
+                        echo "<p> Di kasi manhid na tulad mo </p>";
+                        echo "<p> Alam kong sanay bumitaw </p>";
+                        echo "<p> Ang isang tulad mo </p>";
+                        echo "<p> Lalayo na ba ako </p>";
+                        echo "<br/>";
+
+                        echo "<p> Pano naman ako </p>";
+                        echo "<p> Nahulog na sayo </p>";
+                        echo "<p> Binitawan mo lang ba talaga ako </p>";
+                        echo "<br/>";
+
+                        echo "<p> Pano naman ako </p>";
+                        echo "<p> Naghintay ng matagal sayo </p>";
+                        echo "<p> Wala lang ba talaga lahat ng yon sayo </p>";
+                        echo "<p> Ano na bang gagawin ko </p>";
+                        echo "<br/>";
+
+                        echo "<p> Pano naman ako </p>";
+                        echo "<p> Nahulog na sayo </p>";
+                        echo "<p> Binitawan mo lang ba talaga ako </p>";
+                        echo "<br/>";
+
+                        echo "<p> Pano naman ako </p>";
+                        echo "<p> Naghintay ng matagal sayo </p>";
+                        echo "<p> Wala lang ba talaga lahat ng yon sayo </p>";
+                        echo "<p> Ano na bang gagawin ko </p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jhon Rommel Muhi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
