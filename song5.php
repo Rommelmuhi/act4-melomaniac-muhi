d52c766ce99986f383228640a04ed9e04fcd8457
JhonRommelMuhi <jromdaily@gmail.com>
2022-04-11 10:08:31 +0800

    Fifth song clickable

---
+++ song5.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iMUSIC </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to iMUSIC</h1>
+                <p class="lead">Creating an escape from reality</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Fireflies</h2>";
+                        echo "<p class='lead '>Owl City</p>";
+                
+                        echo "<p> You would not believe your eyes </p>";
+                        echo "<p> If ten million fireflies </p>";
+                        echo "<p> Lit up the world as I fell asleep </p>";
+                        echo "<p> 'Cause they fill the open air </p>";
+                        echo "<p> And leave teardrops everywhere </p>";
+                        echo "<p> You'd think me rude but I would just stand and stare </p>";
+                        echo "<br/>";
+
+                        echo "<p> I'd like to make myself believe that planet Earth turns slowly </p>";
+                        echo "<p> It's hard to say that I'd rather stay awake when I'm asleep </p>";
+                        echo "<p> 'Cause everything is never as it seems </p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause I'd get a thousand hugs </p>";
+                        echo "<p> From ten thousand lightning bugs </p>";
+                        echo "<p> As they tried to teach me how to dance </p>";
+                        echo "<p> A foxtrot above my head </p>";
+                        echo "<p> A sock hop beneath my bed </p>";
+                        echo "<p> A disco ball is just hanging by a thread (thread, thread)</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'd like to make myself believe that planet Earth turns slowly </p>";
+                        echo "<p> It's hard to say that I'd rather stay awake when I'm asleep </p>";
+                        echo "<p> 'Cause everything is never as it seems (when I fall asleep) </p>";
+                        echo "<br/>";
+
+                        echo "<p> Leave my door open just a crack </p>";
+                        echo "<p> Please take me away from here </p>";
+                        echo "<p> 'Cause I feel like such an insomniac </p>";
+                        echo "<p> Please take me away from here </p>";
+                        echo "<p> Why do I tire of counting sheep? </p>";
+                        echo "<p> Please take me away from here </p>";
+                        echo "<p> When I'm far too tired to fall asleep </p>";
+                        echo "<br/>";
+
+                        echo "<p> To ten million fireflies </p>";
+                        echo "<p> I'm weird 'cause I hate goodbyes </p>";
+                        echo "<p> I got misty eyes as they said, Farewell (they said farewell) </p>";
+                        echo "<p> But I'll know where several are </p>";
+                        echo "<p> If my dreams get real bizarre </p>";
+                        echo "<p> 'Cause I saved a few and I keep them in a jar (jar, jar, jar) </p>";
+                        echo "<br/>";
+
+                        echo "<p> I'd like to make myself believe that planet Earth turns slowly </p>";
+                        echo "<p> It's hard to say that I'd rather stay awake when I'm asleep </p>";
+                        echo "<p> 'Cause everything is never as it seems (when I fall asleep) </p>";
+                        echo "<br/>";
+
+                        echo "<p> I'd like to make myself believe that planet Earth turns slowly </p>";
+                        echo "<p> It's hard to say that I'd rather stay awake when I'm asleep </p>";
+                        echo "<p> 'Cause everything is never as it seems (when I fall asleep) </p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jhon Rommel Muhi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
