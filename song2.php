0fc2cae4d432952532345e80c9aefc41b05bbc69
JhonRommelMuhi <jromdaily@gmail.com>
2022-04-11 10:06:17 +0800

    Second song clickable

---
+++ song2.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iMUSIC </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to iMUSIC</h1>
+                <p class="lead">Creating an escape from reality</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Leaves</h2>";
+                        echo "<p class='lead '>Song by Ben&Ben</p>";
+                
+                        echo "<p> I can think of all the times </p>";
+                        echo "<p> You told me not to touch the light </p>";
+                        echo "<p> I never thought that you would be the one </p>";
+                        echo "<p> I couldn't really justify </p>";
+                        echo "<p> How you even thought it could be right </p>";
+                        echo "<p> Cause everything we cherished is gone  </p>";
+                        echo "<p> And in the end, can you tell me if </p>";
+                        echo "<p> It was worth the try, so I can decide </p>";
+                        echo "<br/>";
+
+                        echo "<p> Leaves will soon grow from the bareness of trees </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> From waves overgrown come the calmest of seas </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> Oh you never really love someone until you learn to forgive </p>";
+                        echo "<br/>";
+
+                        echo "<p> Try as hard as I might </p>";
+                        echo "<p> To flee the shadows of the night </p>";
+                        echo "<p> It haunts me and it makes me feel blue </p>";
+                        echo "<p> But how can I try to hide </p>";
+                        echo "<p> When every breath and every hour </p>";
+                        echo "<p> I still end up thinking of you? </p>";
+                        echo "<p> And in the end, everything we have makes it worth the fight </p>";
+                        echo "<p> So I will hold on for as long </p>";
+                        echo "<br/>";
+
+                        echo "<p> As leaves will soon grow from the bareness of trees </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> From waves overgrown come the calmest of seas </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> Oh you never really love someone until you learn to forgive </p>";
+                        echo "<br/>";
+
+                        echo "<p> I never thought that I would see the day </p>";
+                        echo "<p> That I'd decide if I should leave or stay </p>";
+                        echo "<p> But in the end what makes it worth the fights </p>";
+                        echo "<p> That no matter what happens we try to make it right </p>";
+                        echo "<br/>";
+
+                        echo "<p> Leaves will soon grow from the bareness of trees </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> From waves overgrown come the calmest of seas </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> Wounds of the past will eventually heal </p>";
+                        echo "<p> And all will be alright in time </p>";
+                        echo "<p> 'Cause all of this comes with a love that is real </p>";
+                        echo "<p> I said all will be alright in time </p>";
+                        echo "<p> I said all will be alright in time </p>";
+                        echo "<p> I said all will be alright in time </p>";
+                        echo "<p> All will be alright in time </p>";
+                        echo "<p> Oh, you never really love someone until </p>";
+                        echo "<p> You learn to forgive </p>";
+                        echo "<p> You learn to forgive </p>";
+                        echo "<p> Learn to forgive </p>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jhon Rommel Muhi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
